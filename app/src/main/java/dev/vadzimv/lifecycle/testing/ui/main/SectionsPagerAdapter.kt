2283f1c39bdfa6f2b8986e8f95df0d3d74f9ddc0
vadzimv <vysotski.vadim@gmail.com>
2020-05-01 19:34:05 +0300

    initial commit

---
+++ app/src/main/java/dev/vadzimv/lifecycle/testing/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,29 @@
+package dev.vadzimv.lifecycle.testing.ui.main
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import dev.vadzimv.lifecycle.testing.NotSavedState
+import dev.vadzimv.lifecycle.testing.R
+
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
+
+    private val tabs = listOf(
+        Tab(R.string.tab_text_1) { NotSavedState() },
+        Tab(R.string.tab_text_2) { PlaceholderFragment.newInstance(2) }
+    )
+
+    override fun getItem(position: Int): Fragment {
+        return tabs[position].factory()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? =
+        context.resources.getString(tabs[position].nameResource)
+
+    override fun getCount(): Int = tabs.size
+
+    private data class Tab(@StringRes val nameResource: Int, val factory: () -> Fragment)
+}
